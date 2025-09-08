ab503dc5345ad3d2771cf07d690979bd8fd7d68b
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-07 20:29:26 -0800

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,9 @@
+package day1coding;
+public class Day1coding {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("hello world");
+    }
+    
+}
