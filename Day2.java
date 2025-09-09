8e7104d2b00037121ccadce9b4cf61189cdfbbad
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-08 23:16:42 -0800

    Day2.java
    hari ke2 saya membuat penggunaan println(), print(), dan printlf()

---
+++ Day2.java
@@ -0,0 +1,11 @@
+package day2;
+public class Day2 {
+    public static void main(String[] args) {
+        
+        System.out.println("serlimeln");
+        System.out.print(17);
+        
+        System.out.printf("nama:%s, umur:%d", "serli", 17);
+    }
+      
+}
