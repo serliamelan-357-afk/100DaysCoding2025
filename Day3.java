85205635ba8cf115b23b97c4ce4ca52656a55a27
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-09 23:40:42 -0800

    Create Day3.java
    harike3 saya belajar memahami penggunaan komentar

---
+++ Day3.java
@@ -0,0 +1,21 @@
+//nama dari file
+package daya3;
+/**
+ * kelas daya3 adalah contoh program java 
+ * @author serliamela
+ * komentar ini digunakan untuk untuk penjelasan yang lebih rapi dan detail 
+ */
+public class Daya3 {
+  
+    // metode utama yang dijalankan pertama kali 
+    //digunakan komentar yang lebih singkat
+     public static void main(String[] args) { 
+           /*
+           program ini akan menampilkan teks "hello world" ke layar
+           komentar ini digunakan untuk komentar yang lebih panjang
+           */
+         System.out.println("hello wold");
+                 
+    }
+    
+}
