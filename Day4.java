53df45b0d8164f3f7b55b4db8b1c62393d91917a
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-10 23:41:30 -0800

    Day4.java
    pada hari keempa saya belajar menggunakan Escape Sequence (\t,\n)

---
+++ Day4.java
@@ -0,0 +1,18 @@
+package days4;
+public class Days4 {
+    public static void main(String[] args) {
+       System.out.println("nama\t\t: serli ");
+       System.out.println("nim \t\t: D0225017");
+       System.out.println("kelas \t\t: B");
+       System.out.println("prodi\t\t: informatika");
+       System.out.println("jenis kelamin \t:perempuan");
+       
+        System.out.println("nama\t\t:serliamelan \nnim \t\t:D022501 \nkelas \t\t:informatika b \njenis kelamin\t:perempuan");
+        
+        
+       
+       
+    
+    }
+    
+}
