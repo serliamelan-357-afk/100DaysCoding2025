933563e2ca40b8ebbd34542b35a4bbde0edaad52
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-12 23:23:39 -0800

    day006.java
    hari ini saya belajar menggunakan tipe data pecahan (double, float)

---
+++ day006.java
@@ -0,0 +1,12 @@
+package day6;
+public class Day6 {
+    public static void main(String[] args) {
+        
+        double hargapensil = 40.000;
+        float hargabuku = 500f;
+        
+        System.out.println("hargapensil :" + hargapensil);
+        System.out.println("hargbuku :" + hargabuku);
+    }
+    
+}
