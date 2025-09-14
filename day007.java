223a3128f9359695c88488a3602991273293aa6b
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-14 01:15:16 -0800

    day007.java
    hari ini saya belajar mennunakan tipe data karakter char dan boolean.

---
+++ day007.java
@@ -0,0 +1,21 @@
+package day7;
+
+/**
+ *
+ * @author serliamelan
+ */
+public class Day7 {
+    public static void main(String[] args) {
+        char gender = 'p'; 
+        char simbol ='!';
+        boolean  apakah_saya_mahasiswa = true;
+        boolean apakah_sayasiswa_SMA = false;
+        
+        System.out.println (gender);
+        System.out.println(simbol);
+        System.out.println(apakah_saya_mahasiswa);
+        System.out.println(apakah_sayasiswa_SMA);
+        
+    }
+    
+}
