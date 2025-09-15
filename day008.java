e00529a8fdc216b63c7f9d20d12d67ea074bd33b
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-14 19:10:08 -0800

    day008.java
    hari ini saya belajar menggunakan tipe data string

---
+++ day008.java
@@ -0,0 +1,21 @@
+package day8;
+public class Day8 {
+    public static void main(String[] args) {
+        
+        String teks = "nama saya serliamelan"+
+                "string digunakan untuk menyimpan teks atau karakter "+
+                "dengan string kita bisa menggabungkan teks,"+
+                "semakin berlatih, semakin mahir pula kita dalam membuat program";
+        String umur = ("tujuh belas");
+        String prodi = ("informatika");
+        String faultas = ("teknik");
+        
+        System.out.println(teks);
+        System.out.println(umur);
+        System.out.println(prodi);
+        System.out.println(faultas);
+        
+
+    }
+    
+}
