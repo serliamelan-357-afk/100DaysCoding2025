188f370d6a789ea584240aef4e455f05a864650a
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-15 23:44:12 -0800

    day009.java
    hari ini saya belajar    deklarasi, inisialisasi dan update variabel

---
+++ day009.java
@@ -0,0 +1,38 @@
+package day9;
+public class Day9 {
+
+    
+    public static void main(String[] args) {
+        //String  nama = "serliamelan";
+        //int umur ;
+        //umur = 17;
+        //System.out.println(umur);
+        //umur = 12;
+        //System.out.println(umur);
+        
+        String nama = "serli";
+        int umur = 17;
+        char gender = 'p';
+        double tinggibadan = 148.9;
+        
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(gender);
+        System.out.println(tinggibadan);
+        
+        nama = "devin";
+        umur = 18;
+        gender = 'L';
+        tinggibadan = 160.8;
+        
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(gender);
+        System.out.println(tinggibadan);
+        
+        
+        
+        
+    }
+    
+}
