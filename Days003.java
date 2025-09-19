054c216d4f36d9630bcb2dc01e720dc597e5ccb4
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-19 06:01:51 -0800

    Update Days003.java
    hari ini saya belajar penggunaan komentar

--- Days003.java
+++ Days003.java
@@ -1,15 +1,15 @@
 //nama dari file
-package daya3;
+package minggu1;
 /**
  * kelas daya3 adalah contoh program java 
  * @author serliamela
  * komentar ini digunakan untuk untuk penjelasan yang lebih rapi dan detail 
  */
-public class Daya3 {
-  
+public class days3 {
     // metode utama yang dijalankan pertama kali 
     //digunakan komentar yang lebih singkat
-     public static void main(String[] args) { 
+    
+       public static void main(String[] args) { 
            /*
            program ini akan menampilkan teks "hello world" ke layar
            komentar ini digunakan untuk komentar yang lebih panjang
