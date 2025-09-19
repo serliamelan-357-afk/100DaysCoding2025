3a7fe441938066658227c8b25c7d1e9d7a4be06b
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-19 08:05:16 -0800

    Update Days005.java
    hari ini saya belajar menggunakan tipe data numerik bilangan bulat byte,short,int,long

--- Days005.java
+++ Days005.java
@@ -1,16 +1,20 @@
-package day5;
-public class Day5 {
+ package minggu1;
+public class days5 {
     public static void main(String[] args) {
-         
-        byte b = 9;
+          byte b = 9;
         short tinggi = 150;
         int gaji = 50000;
-        long  bulat = 24648L;
+        long  bulat = 24648;
         System.out.println(b);
         System.out.println(tinggi);
         System.out.println(gaji);
         System.out.println(bulat);
                 
-                 }
+    }
     
 }
+
+                
+                 
+    
+
