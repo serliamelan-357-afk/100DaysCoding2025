584516829730e3c27e7faa7a97c1aaa78ef963c9
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-19 05:58:27 -0800

    Update Days002.java
    hari ini saya belajar penggunaan print, println, printf.

--- Days002.java
+++ Days002.java
@@ -1,11 +1,14 @@
-package day2;
-public class Day2 {
+ package minggu1;
+public class days2 {
     public static void main(String[] args) {
-        
         System.out.println("serlimeln");
         System.out.print(17);
         
         System.out.printf("nama:%s, umur:%d", "serli", 17);
     }
-      
+    
 }
+
+    
+      
+
