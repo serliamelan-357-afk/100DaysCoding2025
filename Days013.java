b4014dd5f4d5b59375278d762758cdfd322ee9c2
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-20 12:59:57 +0800

    Days013.java
    hari ini saya evaluasi dan mengerjakann soal 4 nomor

---
+++ Days013.java
@@ -0,0 +1,168 @@
+package latihanserli;
+public class evaluasi {
+    public static void main(String[] args) {
+        byte Umur = 21;
+        int No_absen = 12;
+        short SKS = 144;
+        String Nim = "D0225017";
+        char gender = 'L';
+        float Tinggi = 170.5f;
+        double Berat = 65.4;
+        boolean Status = true;
+        String nama = "budi";
+        long bulat = 144;
+        
+        System.out.println("Umur: " +Umur);
+        System.out.println("SKS: "+SKS);
+        System.out.println("Nim: " +Nim);
+        System.out.println("Tinggi_badan: "+Tinggi);
+        System.out.println("Berat_badan: " +Berat);
+        System.out.println(true);
+        
+        System.out.print("Umur: "+Umur);
+        System.out.print("Jumlah_SKS: "+SKS);
+        System.out.print("Nim: "+Nim);
+        System.out.print("gender: "+gender);
+        System.out.println("Tinggi: "+ Tinggi);
+        System.out.println("Berat: "+Berat);
+        System.out.println(true);
+        
+        System.out.printf("nama:%s\n umur:%d", nama,Umur);
+        System.out.printf("\nSKS:%d\n Nim:%s", SKS,Nim );
+        System.out.printf("\ngender:%c\n Tinggi:%f", gender,Tinggi);
+        System.out.printf("\nBerat:%d\n Status:%b", Berat,Status);
+
+        // soal pertama
+        
+        package latihanserli;
+public class soal2 {
+    public static void main(String[] args) {
+        byte Umur = 21;
+        int No_absen = 12;
+        short Jumlah_SKS = 144;
+        String Nim = "D0225017";
+        char gender = 'L';
+        float  Tinggi_badan = 170.5f;
+        double Berat_badan = 65.4;
+        boolean Satuts_kuliah = true;
+        long bulat = 144;
+        
+        
+        System.out.println(Umur);
+        System.out.println(No_absen);
+        System.out.println(Nim);
+        System.out.println(gender);
+        System.out.println(Tinggi_badan);
+        System.out.println(Berat_badan);
+        System.out.println(true);
+        System.out.println(bulat);
+        
+        
+        Umur = 22;
+        No_absen = 14;
+        Jumlah_SKS = 155;
+        Nim ="D0225018";
+        gender = 'p';
+        Tinggi_badan = 160.4f;
+        Berat_badan = 48.3;
+        Satuts_kuliah = false;
+        bulat = 122;
+        
+        System.out.println(Umur);
+        System.out.println(No_absen);
+        System.out.println(Nim);
+        System.out.println(gender);
+        System.out.println(Tinggi_badan);
+        System.out.println(Berat_badan);
+        System.out.println(false);
+        System.out.println(bulat);
+
+        //soal kedua
+
+        package latihanserli;
+import java.util.Scanner;
+public class soal3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Masukkan_nama\t\t: ");
+        String nama = input.nextLine();
+        
+        System.out.print("Masukkan_umur\t\t: ");
+        byte umur = input.nextByte();
+        
+        System.out.print("Masukkan_noabsen\t: ");
+        int noabsen = input.nextInt();
+        
+        System.out.print("Masukkan_tinggi\t\t: ");
+        double tinggi = input.nextDouble();
+        
+        System.out.print("Masukkan_alamat\t\t: ");
+        input.nextLine();
+        String alamat = input.nextLine();
+        
+        System.out.println("\n===biodata===");
+        System.out.println("Masukkan_nama\t\t: "+nama);
+        System.out.println("Masukkan_umur\t\t: "+umur);
+        System.out.println("Masukkan_noabsen\t: "+noabsen);
+        System.out.println("Masukkan_tingi\t\t: "+tinggi);
+        System.out.println("Masukkan_alamat\t\t: "+alamat);
+        
+        //soal ketiga
+
+        package latihanserli;
+import java.util.Scanner;
+public class soal4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Masukkan_nilai_tugas\t: ");
+        final double nilai_tugas = input.nextDouble();
+        input.nextLine();
+        
+        System.out.print("Masukkan_nama\t\t: ");
+        final String nama = input.nextLine();
+        
+        System.out.print("Masukkan_tangal_lahir\t: ");
+        final int  tanggal_lahir = input.nextInt();
+        
+        
+        System.out.println("\n===biodata===");
+        System.out.println("Masukkan_nilai_tugas\t:" +nilai_tugas);
+        System.out.println("Masukkan_nama\t\t:" +nama);
+        System.out.println("Masukkan_tanggal_lahir\t:" +tanggal_lahir);
+
+        //soal tabahan
+        
+
+        
+    }
+    
+}
+
+                
+        
+    }
+    
+}
+
+        
+        
+
+        
+
+        
+
+
+        
+       
+        
+        
+                
+    
+        
+        
+        
+    }
+    
+}
