7cfa58af556cc67f4549ea93c8a633ad13045cc5
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-19 16:10:11 -0800

    Update and rename day010.java to Days010.java
    hari ini saya belajar menggunakan final variabel

---
+++ Days010.java
@@ -0,0 +1,22 @@
+ package minggu1;
+public class days10 {
+    public static void main(String[] args) {
+        final String nama = "serliamelan";
+        final int umur = 17;
+        final double nilai_rapor = 87.5;
+        
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(nilai_rapor);
+        
+        //jika mencoba mengubah nilai akan eror
+        
+    
+    
+
+        
+        
+        
+            }
+    
+}
