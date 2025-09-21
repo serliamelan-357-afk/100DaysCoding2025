4114c176ebe083d64ccac9b7722ae87083940c63
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-21 16:59:10 +0800

    Days014.java
    hari ini saya belajar menggunakan mengubah string ke tipe data primitif

---
+++ Days014.java
@@ -0,0 +1,24 @@
+package minggu2;
+import java.util.Scanner;
+public class Days014 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.print("Masukkan_sebuah_string_25: ");
+        String input = scanner.nextLine();
+        
+        int nilai = Integer.parseInt(input);
+        System.out.println("Hasil_konversi_ke_int: " +nilai);
+         
+         nilai = 20;
+         System.out.println("Update_nilaintI (+20): " +nilai);
+         
+         System.out.print("Masukkan_sebuah_string_(20.5): ");
+         input = scanner.nextLine();
+        
+         
+
+        
+    }
+    
+}
