f78da1cc7fe43b2fcc2d3fb658513a46103c5e0a
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-23 19:10:01 +0800

    Days016.java
    hari ini saya belajar menggunakan operator aritmatika perkalian dan pembagian

---
+++ Days016.java
@@ -0,0 +1,28 @@
+package minggu2;
+import java.util.Scanner;
+public class Days016 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan angka3: ");
+        int angka3 = input.nextInt();
+        
+        System.out.print("masukan angka4: ");
+        int angka4 = input.nextInt();
+        
+        int kali = angka3 * angka4;
+        int bagi  = angka3 / angka4;
+        
+        System.out.println("hasil kali:" + kali);
+        System.out.println("hasil bagi:" + bagi);
+       
+        
+        
+    }
+ 
+        
+    
+    
+}
+        
+    
+    
