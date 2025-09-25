f14639567c610bd757f7819b9afae3123ceb0959
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-25 12:22:01 +0800

    Days018.java
    hari ini saya belajar menggunakan koneversi otomatis (tipe data kecil ke besar.

---
+++ Days018.java
@@ -0,0 +1,13 @@
+package minggu2;
+public class Days018 {
+    public static void main(String[] args) {
+        
+        int angkakecil = 90;
+        double angkabesar = angkakecil;
+        
+        System.out.println("Nilai int:" +angkakecil);
+        System.out.println("Nilai double:" +angkabesar);
+        
+    }
+    
+}
