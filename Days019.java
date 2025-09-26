bc0d1206693acc8738a32ac07279c46b1004cd73
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-26 23:12:24 +0800

    Days019.java

---
+++ Days019.java
@@ -0,0 +1,23 @@
+package minggu2;
+
+public class Days019 {
+
+    public static void main(String[] args) {
+        int angkakecil;
+        double angkadouble = 25.3;
+
+        angkakecil = (int) angkadouble;
+
+        System.out.println("Nilai double: " +angkadouble);
+        System.out.println("Nilai int: " +angkakecil);
+        
+        
+        
+        
+        
+        
+       
+
+    }
+
+}
