676b1965615548e9506098030ee639bff7d7f409
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-27 20:43:15 +0800

    Days020.java
    hari ini saya belajar konversi tipe primitif ke String

---
+++ Days020.java
@@ -0,0 +1,24 @@
+package minggu2;
+public class Days020 {
+    public static void main(String[] args) {
+        
+        int angka = 20;
+        double pecahan = 3.14;
+        boolean status = true;
+        
+        String serli = String.valueOf(angka);
+        String amelan = String.valueOf(pecahan);
+        String terserah = String.valueOf(status);
+        
+        System.out.println(serli);
+        System.out.println(amelan);
+        System.out.println(terserah);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
