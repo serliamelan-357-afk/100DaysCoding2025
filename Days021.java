102cdab65d579ae692e43ceb92f3438b9ecdae3c
serliamelan-357-afk <amelanserli@gmail.com>
2025-09-28 20:56:43 +0800

    Days021.java
    hari ini saya latihan menukar nilai dua variabel tanpa mengunakan variabel tambahan

--- Days021.java
+++ Days021.java
@@ -1,16 +1,24 @@
-package minggu2;
+  package minggu2;
 public class Days021 {
     public static void main(String[] args) {
-        int a = 20, b = 10;
-        System.out.println("sebelum ditukar: a = " + a + ",b = " + b);
-
-        int temp = a;
-        a = b;
-        b = temp;
-
-        System.out.println("sesudah ditukar: a = " + a + ", b = " + b);
+        int angka1 = 20;
+        int angka2 = 50;
+        
+        System.out.println("sebelum ditukar: ");
+        System.out.println("angka1 = " + angka1);
+        System.out.println("angka2 = " + angka2);
+        
+        angka1 = angka1 + angka2;
+        angka2 = angka1 - angka2;
+        angka1 = angka1 - angka2;
+        
+        System.out.println("\nSesudah ditukar: ");
+        System.out.println("angka1 = " + angka1);
+        System.out.println("angka2 = " + angka2);
        
 
     }
 
 }
+
+    
