e49a5696a55f117e2eff6eee6543d1782de3d40c
serliamelan-357-afk <amelanserli@gmail.com>
2025-10-02 20:46:06 +0800

    Days025.java
    hari ini saya membuat program dengan menggunakan operator Increment dan Decrement saya pake dua metode yaitu tambah dulu baru pakai pakai dulu baru tambah, brgitupun juga dengan kurang.

---
+++ Days025.java
@@ -0,0 +1,20 @@
+package minggu2;
+
+public class Days025 {
+    public static void main(String[] args) {
+        
+        int a = 7; 
+        a++;
+        a--;
+        
+        System.out.println(a++);
+        System.out.println(a--);
+        
+        System.out.println(++a);
+        System.out.println(--a);
+       
+        System.out.println(a);
+        
+    }
+
+}
