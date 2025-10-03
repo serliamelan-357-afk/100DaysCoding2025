30c1990b8d1672a44639b2c83419c243f3eb4e7d
serliamelan-357-afk <amelanserli@gmail.com>
2025-10-03 22:15:54 +0800

    Days026.java
    pada hari ini saya belajar menggunakan operator penugasan 
    +=, -=, *=, /=, %=

---
+++ Days026.java
@@ -0,0 +1,23 @@
+package minggu2;
+
+public class Days026 {
+
+    public static void main(String[] args) {
+
+        int X = 5, v = 3,
+                n = 2, W = 5, S = 7;
+        X += 4;
+        v -= 3;
+        n *= 2;
+        W /= 5;
+        S %= 2;
+        
+
+        System.out.println("hasil x +=: " + X);
+        System.out.println("hasil v -=: " + v);
+        System.out.println("hasil n *=: " + n);
+        System.out.println("hasil w /=: " + W);
+        System.out.println("hasil S%=: " + S);
+    }
+
+}
