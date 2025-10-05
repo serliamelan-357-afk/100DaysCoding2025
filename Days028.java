d7ecdb970e24496cf6cca824093ae7f346e23521
serliamelan-357-afk <amelanserli@gmail.com>
2025-10-05 19:26:10 +0800

    Days028.java
    haari ini saya belajar menggunakan operator perbandingan == dan != dengan menggunakan tipe data integer

---
+++ Days028.java
@@ -0,0 +1,13 @@
+package minggu3;
+
+public class Days028 {
+    public static void main(String[] args) {
+        
+        int y = 2;
+        int v = 4;
+        
+        System.out.println(y == v);
+        System.out.println(y != v);
+    }
+
+}
