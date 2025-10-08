addbe5f3a33ef20e264945ec7e814ffa24344895
serliamelan-357-afk <amelanserli@gmail.com>
2025-10-08 20:34:27 +0700

    Days031.java
    hari ini saya belajar menggunakan operator perbandingan AND (&&)

---
+++ Days031.java
@@ -0,0 +1,16 @@
+package minggu3;
+
+public class Days031 {
+
+    public static void main(String[] args) {
+        
+        int nilai = 80;
+        int kehadiran = 90;
+        
+        boolean lulus = (nilai >= 70) && (kehadiran >= 80);
+        
+        System.out.println("status kelulusan: " + lulus);
+
+    }
+
+}
