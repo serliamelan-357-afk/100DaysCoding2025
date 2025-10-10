acfb89fb90bd222ba093e70ac75292b219a8be4d
serliamelan-357-afk <amelanserli@gmail.com>
2025-10-10 21:48:12 +0800

    Days033.java
    hari ini saya belajar menggunakan operator logika  NOT

---
+++ Days033.java
@@ -0,0 +1,14 @@
+package minggu3;
+
+public class Days033 {
+
+    public static void main(String[] args) {
+        
+        int angka = 10; 
+        
+        System.out.println("Angka lebih besar dari 5: " + (angka > 5));
+        System.out.println("kebalikanya: " + !(angka > 5));
+
+    }
+
+}
