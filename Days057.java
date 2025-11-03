6bd9413229e5aaa7b68ce5bfe2d5996c40a7baf2
serliamelan-357-afk <amelanserli@gmail.com>
2025-11-03 22:09:30 +0800

    Days057.java
    hari ini saya belajar menggunaka nested loop

---
+++ Days057.java
@@ -0,0 +1,18 @@
+
+package minggu5;
+
+public class Days057 {
+    public static void main(String[] args) {
+         
+        for (int i = 1; i <= 10; i++) {
+          
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i * j + "\t");
+                
+            }
+            System.out.println("");
+        }
+       
+    }
+
+}
