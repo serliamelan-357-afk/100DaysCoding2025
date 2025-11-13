87ea40578bdeaebfa5810771b2e722501d700464
serliamelan-357-afk <amelanserli@gmail.com>
2025-11-13 22:23:31 +0800

    Days066..java
    hari ini saya latihan mencetak pola bintang vertikal

---
+++ Days066..java
@@ -0,0 +1,20 @@
+package minggu5;
+
+import java.util.Scanner;
+
+public class Days066 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Masukkan nilai n: ");
+        int n = s.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*");
+            
+        }
+        System.out.println();
+        
+    }
+
+}
