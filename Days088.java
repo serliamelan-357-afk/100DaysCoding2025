e4e7a652eb0eb622b970eb29fd604690442ee65f
serliamelan-357-afk <amelanserli@gmail.com>
2025-12-04 21:53:57 +0800

    Days088.java

---
+++ Days088.java
@@ -0,0 +1,15 @@
+package minggu7;
+
+public class Days088 {
+    public static void main(String[] args) {
+        
+        int[] angka = {10, 20, 30, 40, 50};
+        
+        System.out.println("Isi array");
+        for (int a : angka) {
+            System.out.print(a);
+            
+        }
+    }
+
+}
