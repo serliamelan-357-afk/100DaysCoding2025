c768ee13005bc2bb0275439c8dfb4a97c29e6c7b
serliamelan-357-afk <amelanserli@gmail.com>
2025-12-05 08:27:30 +0800

    Days089.java

---
+++ Days089.java
@@ -0,0 +1,15 @@
+package minggu7;
+
+public class Days089 {
+    public static void main(String[] args) {
+        cetakAssalamualaikum();
+        cetakPesan();
+        
+    }
+    public static void cetakAssalamualaikum(){
+        System.out.println("Assalamualaikum");
+    }
+    public static void cetakPesan(){
+        System.out.println("perkenalkan nama saya serliamelan");
+    }
+}
