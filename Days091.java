14e663b659e9a6e7cb4ea8358d271419ca47105c
serliamelan-357-afk <amelanserli@gmail.com>
2025-12-07 20:38:38 +0800

    Days091.java

---
+++ Days091.java
@@ -0,0 +1,15 @@
+package minggu7;
+
+public class Dyas091 {
+    public static void main(String[] args) {
+        tambah(10, 5);
+        tambah(7, 5);
+        
+        
+    }
+    public static void tambah(int a, int b){
+        int hasil = a + b;
+        System.out.println("Hasil: " + hasil);
+    }
+
+}
