6648c0abbe7e60a3819a220be6c4c651a0082670
serliamelan-357-afk <amelanserli@gmail.com>
2025-12-09 21:06:20 +0800

    Days093.java

---
+++ Days093.java
@@ -0,0 +1,20 @@
+package minggu7;
+
+public class Days093 {
+    public static void main(String[] args) {
+        
+        double angka1 = 7.2;
+        double angka2 = 7.8;
+        double angka3 = 7.5;
+        
+        System.out.println("===CEIL===");
+        System.out.println("Ceil dari " + angka1 + " = " +Math.ceil(angka1));
+        
+        System.out.println("\n=== FLOOR ===");
+        System.out.println("Floor dari " + angka2 + " = " + Math.floor(angka2));
+        
+        System.out.println("\n=== ROUND ===");
+        System.out.println("Round dari " + angka3 + " = " + Math.round(angka3) );
+    }
+
+}
