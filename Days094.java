c758089d63df4fe4836147d620ed3ecb6c0f8f37
serliamelan-357-afk <amelanserli@gmail.com>
2025-12-10 20:09:10 +0800

    Days094.java

---
+++ Days094.java
@@ -0,0 +1,15 @@
+package minggu7;
+
+public class Days094 {
+    public static void main(String[] args) {
+        cetakHasil(7, 8);
+        cetakHasil(11, 2);
+        
+        
+    }
+    public static void cetakHasil(int a, int b){
+        int hasil = a + b;
+        System.out.println("Hasil Penjumlahan: " + hasil);
+    }
+
+}
