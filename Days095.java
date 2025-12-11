5a19b5afe235de2af538c7c77ef70f566efc7593
serliamelan-357-afk <amelanserli@gmail.com>
2025-12-11 19:52:30 +0800

    Days095.java

---
+++ Days095.java
@@ -0,0 +1,16 @@
+package minggu7;
+
+public class Days095 {
+    public static void main(String[] args) {
+        cetakData("Serli amelan", 17, "Sendana");
+        cetakData("Rusmiati", 18, "Pasangkayu");
+        
+    }
+    public static void cetakData(String nama, int umur, String alamat){
+        System.out.println("Nama   : " + nama);
+        System.out.println("Umur   : " + umur);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("---------------------");
+    }
+
+}
