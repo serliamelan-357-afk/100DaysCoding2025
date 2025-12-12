cd784c8a83a5d49a3f7f6cb37029dabc9d372385
serliamelan-357-afk <amelanserli@gmail.com>
2025-12-12 20:13:25 +0800

    Days096.java

---
+++ Days096.java
@@ -0,0 +1,39 @@
+package minggu7;
+
+public class Days096 {
+
+    public static void main(String[] args) {
+        
+        int a = 12;
+        int b = 2;
+        
+        
+
+        System.out.println("Angka 1 : " + a);
+        System.out.println("Angka 2 : " + b);
+
+        System.out.println("\n==HASIL OPERASI==");
+        System.out.println("Penjumlahan : " + tambah(a, b));
+        System.out.println("Pengurangan : " + kurang(a, b));
+        System.out.println("Perkalian   : " + kali(a, b));
+        System.out.println("Pembagian   : " + bagi(a, b));
+
+    }
+
+    public static int tambah(int x, int y) {
+        return x + y;
+    }
+
+    public static int kurang(int x, int y) {
+        return x - y;
+    }
+
+    public static int kali(int x, int y) {
+        return x * y;
+    }
+
+    public static int bagi(int x, int y) {
+        return x / y;
+    }
+
+}
